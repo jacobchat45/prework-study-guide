47bbe3b808a589819aeba96934dc2927d0159908
Jacob Chatfield <jacob.chatfield45@outlook.com>
2022-11-08 17:12:42 +0000

    Added for loops and arrays

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,12 @@
+
+ /*  var shapes = ["triangle", "square", "pentagon", "circle"];
+   for(let x = 0; x < shapes.length; x++){
+    console.log(shapes[x]);
+   };
+*/
+
+let topics = ["HTML", "CSS", "Git", "Javascript"];
+
+for(x = 0; x < topics.length; x++){
+    console.log(topics[x]);
+};
